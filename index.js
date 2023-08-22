8a5d5ea302766a07f46b1ea42b0a51c293b631a2
SharpeurNes <sharpeurnes@gmail.com>
2023-08-22 14:11:39 +0200

    debut projet

---
+++ index.js
@@ -0,0 +1,16 @@
+const {Client, GatewayIntentBits, Partials} = require('discord.js');
+
+const {Guilds, GuildMembers, GuildMessages} = GatewayIntentBits;
+const {User, Message, GuildMember, ThreadMember, Channel} = Partials;
+
+const client = new Client({
+    intents: [Guilds, GuildMembers, GuildMessages],
+    partials: [User, Message, GuildMember, ThreadMember],
+});
+
+client.once('ready', () => {
+    console.log('Connected !')
+});
+
+client.config = require('./config.json');
+client.login(client.config.token);
